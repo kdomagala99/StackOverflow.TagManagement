cb3597d2866efadf67e7ace498866336b6b782a9
Kacper Domagala <kacperdomagala99@gmail.com>
2024-04-03 23:30:13 +0200

    feat: Add integration test

---
+++ src/StackOverflow.TagManagement.Api/StackOverflow.TagManagement.Api.Tests/MongoDbContextTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using StackOverflow.TagManagement.Api.Configurations;
+using StackOverflow.TagManagement.Api.Database;
+
+namespace StackOverflow.TagManagement.Api.Tests
+{
+    [TestClass]
+    public class MongoDbContextTests
+    {
+        private string connectionString;
+        private string databaseName;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this.connectionString = "mongodb://localhost:27017";
+            this.databaseName = $"test_db{Guid.NewGuid()}";
+        }
+
+        [TestMethod]
+        public void MongoCreateDatabase_ShouldCreate_ThenDrop()
+        {
+            this.dbContext = new MongoDbContext(new MongoDbConfiguration() { ConnectionString = this.connectionString, DatabaseName = this.databaseName });
+            var client = new MongoClient(this.connectionString);
+            client.DropDatabase(this.databaseName);
+        }
+    }
+}
